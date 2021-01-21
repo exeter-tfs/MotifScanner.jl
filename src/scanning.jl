3d443d0c5194eb9f4593d72e347c7f9e44e8e4bd
owensnick <owensnick@gmail.com>
2021-01-21 21:11:09 +0000

    motif scanning

--- src/scanning.jl
+++ src/scanning.jl
@@ -2,4 +2,36 @@
 ## reverse complement
 rcm(M) = reverse(reverse(M, dims=1), dims=2)
 
+
+### loop for forward and reverse motif scanning
+## no doubt there are efficiency savings available here
+function scanmotif(seq, mot)
+    n = length(seq)
+    m = size(mot, 2)
+    fscores = zeros(Float64, n)
+    rscores = zeros(Float64, n)
+    rot = rcm(mot)
+    for i = 1:(n-m)
+        for j = 1:m
+            if seq[i + j - 1] == DNA_A
+                fscores[i] += mot[1, j]
+                rscores[i] += rot[1, j]
+            elseif seq[i + j - 1] == DNA_C
+                fscores[i] += mot[2, j]
+                rscores[i] += rot[2, j]
+            elseif seq[i + j - 1] == DNA_G
+                fscores[i] += mot[3, j]
+                rscores[i] += rot[3, j]
+            elseif seq[i + j - 1] == DNA_T
+                fscores[i] += mot[4, j]
+                rscores[i] += rot[4, j]
+            end
+        end
+        
+    end
+    
+    
+    fscores, rscores
+end
+
 ##
