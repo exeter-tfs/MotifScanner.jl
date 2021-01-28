bbd06f7e3c79749c97cf17366ab8cd966e823814
owensnick <owensnick@gmail.com>
2021-01-28 09:59:00 +0000

    Added function exports

--- src/MotifScanner.jl
+++ src/MotifScanner.jl
@@ -4,6 +4,8 @@ using BioSequences
 using Plots
 using StatsBase
 
+export loadmeme, scanmotif, scanmotstats, consensus, plotseq, plotletter!, seqlogo!, seqlogo
+
 include("loadmeme.jl")
 include("scanning.jl")
 include("seqlogodata.jl")
