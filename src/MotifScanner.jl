2db2d2db50a4b211f2e91c0f05d6ce565407de1a
owensnick <owensnick@gmail.com>
2021-01-28 10:14:28 +0000

    Added DataFrames dependency

--- src/MotifScanner.jl
+++ src/MotifScanner.jl
@@ -3,6 +3,7 @@ module MotifScanner
 using BioSequences
 using Plots
 using StatsBase
+using DataFrames
 
 export loadmeme, scanmotif, scanmotstats, consensus, plotseq, plotletter!, seqlogo!, seqlogo
 
