368c588b207ddf0de5e4a2aa10fdcb7bc96c1c81
owensnick <owensnick@gmail.com>
2021-05-14 15:54:44 +0100

    Add function to loadhomer motifs

--- src/loadmeme.jl
+++ src/loadmeme.jl
@@ -1,6 +1,6 @@
 
-background_human_zero_markov() = [0.2953, 0.2047, 0.2047, 0.2953]
-
+background_human_zero_markov()   = [0.2953, 0.2047, 0.2047, 0.2953]
+background_xenopus_zero_markov() = [0.2996, 0.2004, 0.2004, 0.2996]
 
 ### Load meme into named tuple
 function loadmeme(file, w = 1e-2, background=background_human_zero_markov())
@@ -33,4 +33,32 @@ function loadmeme(file, w = 1e-2, background=background_human_zero_markov())
     
     pbg = log2.(pwm) .- log2.(background)
     (name=motifname, id=motifid, pwm=pwm, pbg=pbg)
+end
+
+function loadhomer(file, w = 1e-2, background=background_human_zero_markov())
+
+    io = open(file)
+    line = readline(io)
+
+    fields = split(line)
+    consensus = fields[1][2:end]
+    longname = fields[2]
+    name = first(split(longname, "/"))
+    sct = parse(Float64, fields[3])
+
+
+    data = Vector{Vector{Float64}}()
+    for line in eachline(io)
+        v = parse.(Float64, split(line))
+        push!(data, v)
+    end
+
+
+
+    pwm = reduce(hcat, data)
+    pwm .+= w
+    pwm ./= sum(pwm, dims=1)
+    
+    pbg = log2.(pwm) .- log2.(background)
+    (name=name, id=longname, pwm=pwm, pbg=pbg, sct=sct, consensus=consensus)
 end
