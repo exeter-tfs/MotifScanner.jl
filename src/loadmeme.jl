22e1d6bf4a103cb1f458c7edca7222f2879c998a
Nick Owens <owensnick@gmail.com>
2021-08-05 20:36:44 +0100

    Added load meme library function

--- src/loadmeme.jl
+++ src/loadmeme.jl
@@ -35,6 +35,64 @@ function loadmeme(file, w = 1e-2, background=background_human_zero_markov())
     (name=motifname, id=motifid, pwm=pwm, pbg=pbg)
 end
 
+
+function loadmemelibrary(file, w = 1e-2, background=background_human_zero_markov())
+
+    motifnames = String[]
+    motifids = String[]
+    motiflengths = Int[]
+    nsites = Int[]
+    mode = :notreading
+    currentmotif = Vector{Vector{Float64}}()
+    pwms = Vector{Matrix{Float64}}()
+    
+    for line in eachline(file)
+        if mode == :notreading
+            if startswith(line, "MOTIF")
+                
+                fields = split(line)
+                motifid   = fields[2]
+                motifname = fields[3]
+                
+                push!(motifnames, motifname)
+                push!(motifids, motifid)
+                
+                mode = :reading
+            end
+        elseif mode == :reading
+            
+            if startswith(line, "letter-probability")
+                currentmotif = Vector{Vector{Float64}}()
+                ldata = replace(line, "letter-probability matrix: " => "")
+                fields = split.(split(replace(ldata, "= " => "=")), "=")
+                
+                dd = Dict(k => v for (k, v) in fields)
+                push!(motiflengths, parse(Int, dd["w"]))
+                push!(nsites, parse(Int, dd["nsites"]))
+
+            elseif startswith(line, "URL")
+                mode = :notreading
+                pwm = reduce(hcat, currentmotif)
+                pwm .+= w
+                pwm ./= sum(pwm, dims=1)
+                push!(pwms, pwm)
+                
+            else
+                v = parse.(Float64, split(line))
+                push!(currentmotif, v)
+            end
+        end
+    end
+
+    motifmeta = DataFrame(MotifName=motifnames, MotifID=motifids, Lengths=motiflengths, NumSites=nsites)
+    pbgs = [log2.(pwm) .- log2.(background) for pwm in pwms]
+
+    motifdata = [(name=motifname, id=id, pwm=pwm, pbg=pbg) for (motifname, id, pwm, pbg) in zip(motifnames, motifids, pwms, pbgs)]
+
+    motifmeta, motifdata
+
+end
+
 function loadhomer(file, w = 1e-2, background=background_human_zero_markov())
 
     io = open(file)
