c98a1977463fbb331cf78418f4b53021c587d38b
Nick Owens <owensnick@gmail.com>
2022-01-21 12:25:37 +0000

    deps and exports

--- src/MotifScanner.jl
+++ src/MotifScanner.jl
@@ -6,10 +6,11 @@ using StatsBase
 using DataFrames
 import PyPlot
 using FASTX
+using ProgressMeter
 
 ## This is a package for scanning motifs in DNA sequences and plotting sequence motifs.
 
-export loadmeme, loadmemelibrary, loadhomer, scanmotif, scanmotstats, consensus, plotseq, plotletter!, seqlogo!, seqlogo, loadrefseqs, motifscanall
+export loadmeme, loadmemelibrary, loadhomer, scanmotif, scanmotstats, consensus, plotseq, plotletter!, seqlogo!, seqlogo, loadrefseqs, motifscanall, loadtransfac
 
 include("loadmeme.jl")
 include("scanning.jl")
